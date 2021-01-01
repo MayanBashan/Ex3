eecd75cd314447442a240abcce617182e9080b6c
yuval1929 <yuval1929@gmail.com>
2021-01-01 02:12:19 +0200

    added documentation to DiGraph

--- src/node_data.py
+++ src/node_data.py
@@ -1,18 +1,17 @@
 class NodeData:
 
-    def __init__(self, key: int, info: str = None, tag: float = 0, x: float = 0, y: float = 0):
-        self.x = x
-        self.y = y
+    def __init__(self, key: int, info: str = None, tag: float = 0, pos: tuple = None):
         self.info = info
         self.tag = tag
         self.key = key
         self.edges_from_node = {}
         self.edges_to_node = {}
+        self.pos = pos
 
-    def add_edge_from_node(self, ni_key, weight):
+    def add_edge_from_node(self, ni_key: int, weight: float):
         self.edges_from_node[ni_key] = weight
 
-    def add_edge_to_node(self, ni_key, weight):
+    def add_edge_to_node(self, ni_key: int, weight: float):
         self.edges_to_node[ni_key] = weight
 
     def get_edges_from_node(self):
@@ -21,7 +20,7 @@ class NodeData:
     def get_edges_to_node(self):
         return self.edges_to_node
 
-    def get_edge_weight(self, ni_key):
+    def get_edge_weight(self, ni_key: int):
         return self.edges_from_node.get(ni_key)
 
     def get_key(self):
@@ -33,12 +32,12 @@ class NodeData:
     def get_tag(self):
         return self.tag
 
-    def get_x(self):
-        return self.x
+    def get_pos(self):
+        return self.pos
 
-    def get_y(self):
-        return self.y
+    def set_pos(self, pos: tuple):
+        self.pos = pos
 
-    def set_tag(self, tag):
+    def set_tag(self, tag: float):
         self.tag = tag
 
